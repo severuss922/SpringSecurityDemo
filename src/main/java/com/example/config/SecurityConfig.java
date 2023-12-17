c71fe60213c6b3d8f7c982dfc612e42606537537
martin <mmarquez210@gmail.com>
2023-12-17 18:26:12 -0300

    agregadas todas las funcionalidades de spring sec y jwt token con base de datos incluida

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -1,18 +1,26 @@
 package com.example.config;
 
+import com.example.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authenticationProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -21,7 +29,9 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authRequest -> authRequest
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated())
-                .formLogin(withDefaults())
+                .sessionManagement( sessionManager -> sessionManager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
--- src/main/java/com/example/controller/AuthController.java
+++ src/main/java/com/example/controller/AuthController.java
@@ -1,6 +1,13 @@
 package com.example.controller;
 
+import com.example.dto.AuthResponseDto;
+import com.example.dto.LoginRequestDto;
+import com.example.dto.RegisterRequestDto;
+import com.example.service.AuthServiceImpl;
+import com.example.service.IAuthService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,13 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
 
+    IAuthService service;
+
+    public AuthController(AuthServiceImpl service){
+        this.service = service;
+    }
+
     @PostMapping("/login")
-    public String login(){
-        return "login from public endpoint";
+    public ResponseEntity<?> login(@RequestBody LoginRequestDto loginRequestDto){
+        return ResponseEntity.ok(service.login(loginRequestDto));
     }
 
     @PostMapping("/register")
-    public String register(){
-        return "Register from public endpoint";
+    public ResponseEntity<?> register(@RequestBody RegisterRequestDto registerRequestDto){
+        return ResponseEntity.ok(service.register(registerRequestDto));
     }
 }
